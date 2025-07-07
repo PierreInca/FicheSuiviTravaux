fdd6b8cfbfbf0ad3c7241cd1074bb56bdd638831
PierreInca <pierre.rapp@inca-ing.lu>
2025-07-07 17:02:05 +0200

    Update implementer_fiche.py

--- implementer_fiche.py
+++ implementer_fiche.py
@@ -1,5 +1,7 @@
 import json
 
+st.write("--- Début de impl_fiche.py ---")
+
 def implementer_num_fiche(nouveau_num, chantier_nom, data):
     data[chantier_nom]["nb_fiches"] = nouveau_num
     # Facultatif : enregistrer dans le fichier si tu veux que ça persiste
